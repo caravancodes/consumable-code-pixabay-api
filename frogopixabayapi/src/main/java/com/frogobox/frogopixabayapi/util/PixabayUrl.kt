eb2e9dd7fc7c624655d33fc1b350b165c9e5370b
amirisback <faisalamircs@gmail.com>
2020-03-14 12:38:25 +0700

    add: empty kotlin file

---
+++ frogopixabayapi/src/main/java/com/frogobox/frogopixabayapi/util/PixabayUrl.kt
@@ -0,0 +1,19 @@
+package com.frogobox.frogopixabayapi.util
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * PixabayAPI
+ * Copyright (C) 14/03/2020.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * LinkedIn : linkedin.com/in/faisalamircs
+ * -----------------------------------------
+ * FrogoBox Software Industries
+ * com.frogobox.frogopixabayapi.util
+ *
+ */
