fbc9b5e26e4303f09abfc00a3d3341b4fde28550
amirisback <faisalamircs@gmail.com>
2020-05-16 19:27:10 +0700

    add: sample code

--- app/src/main/java/com/frogobox/pixabayapi/MainActivity.kt
+++ app/src/main/java/com/frogobox/pixabayapi/MainActivity.kt
@@ -2,12 +2,16 @@ package com.frogobox.pixabayapi
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.frogobox.frogopixabayapi.ConsumePixabayApi
 import com.frogobox.frogopixabayapi.callback.PixabayResultCallback
-import com.frogobox.frogopixabayapi.data.model.Image
+import com.frogobox.frogopixabayapi.data.model.PixabayImage
 import com.frogobox.frogopixabayapi.data.response.Response
 import com.frogobox.frogopixabayapi.util.PixabayConstant
+import com.frogobox.recycler.boilerplate.viewrclass.FrogoViewAdapterCallback
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,12 +20,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupConsumablePixabayAPI("naruto")
+
+    }
+
+    private fun consumePixabay() : ConsumePixabayApi {
         val consumePixabayApi = ConsumePixabayApi(PixabayConstant.SAMPLE_API_KEY) // Your API Key
         consumePixabayApi.usingChuckInterceptor(this) // Using Chuck Interceptor
+        return consumePixabayApi
+    }
 
-        val query = "Nature"
-
-        consumePixabayApi.searchImage(
+    private fun setupConsumablePixabayAPI(query: String){
+        consumePixabay().searchImage(
             query,
             null,
             null,
@@ -36,9 +46,10 @@ class MainActivity : AppCompatActivity() {
             null,
             null,
             null,
-            object : PixabayResultCallback<Response<Image>> {
-                override fun getResultData(data: Response<Image>) {
+            object : PixabayResultCallback<Response<PixabayImage>> {
+                override fun getResultData(data: Response<PixabayImage>) {
                     // Place your UI / Data
+                    data.hits?.let { setupFrogoRecyclerView(it) }
                 }
 
                 override fun failedResult(statusCode: Int, errorMessage: String?) {
@@ -47,15 +58,40 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onShowProgress() {
                     // showing your progress view
-                    progressView.visibility = View.VISIBLE
-                    progressView.text = "Loading Fetching Pixabay API"
+                    runOnUiThread {
+                        progressView.visibility = View.VISIBLE
+                        progressView.text = "Loading Fetching Pixabay API"
+                    }
                 }
 
                 override fun onHideProgress() {
                     // hide your progress view
-                    progressView.visibility = View.GONE
+                    runOnUiThread {
+                        progressView.visibility = View.GONE
+                    }
                 }
             })
+    }
 
+    private fun setupFrogoRecyclerView(data: List<PixabayImage>){
+        frogo_rv.injector<PixabayImage>()
+            .addData(data)
+            .addCustomView(R.layout.frogo_rv_grid_type_12)
+            .addEmptyView(null)
+            .addCallback(object : FrogoViewAdapterCallback<PixabayImage>{
+                override fun onItemClicked(data: PixabayImage) {}
+
+                override fun onItemLongClicked(data: PixabayImage) {}
+
+                override fun setupInitComponent(view: View, data: PixabayImage) {
+                    val ivPoster = view.findViewById<ImageView>(R.id.frogo_rv_type_12_iv_poster)
+                    val tvTitle = view.findViewById<TextView>(R.id.frogo_rv_type_12_tv_title)
+                    Glide.with(view.context).load(data.previewURL).into(ivPoster)
+                    val totalLikes = data.likes.toString()
+                    tvTitle.text = "$totalLikes likes"
+                }
+            })
+            .createLayoutGrid(2)
+            .build()
     }
 }
